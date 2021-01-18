579acf318ddadde7b4f2d2eeea6d860ae4e1d7d1
Parkduksung <duksung1234@naver.com>
2021-01-18 22:48:14 +0900

    기본적인 video 녹화

--- app/src/main/java/com/example/videoprocessing/MainActivity.kt
+++ app/src/main/java/com/example/videoprocessing/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.example.videoprocessing
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import androidx.databinding.DataBindingUtil
+import com.example.videoprocessing.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val REQUEST_VIDEO_CAPTURE = 1
+    }
+
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.lifecycleOwner = this
+        setContentView(binding.root)
+        dispatchTakeVideoIntent()
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_VIDEO_CAPTURE && resultCode == RESULT_OK) {
+            intent.data?.let { binding.videoView.setVideoURI(it) }
+        }
+    }
+
+    private fun dispatchTakeVideoIntent() {
+        Intent(MediaStore.ACTION_VIDEO_CAPTURE).also { takeVideoIntent ->
+            takeVideoIntent.resolveActivity(packageManager)?.also {
+                startActivityForResult(takeVideoIntent, REQUEST_VIDEO_CAPTURE)
+            }
+        }
+    }
+
 }
