33d78eb3b34d73d3590f1c376be5e275f255f84c
Parkduksung <duksung1234@naver.com>
2021-03-18 16:52:33 +0900

    [mediaCodec] 기본세팅.

---
+++ app/src/main/java/com/example/videoprocessing/mediacodec/MediaCodecActivity.kt
@@ -0,0 +1,15 @@
+package com.example.videoprocessing.mediacodec
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.videoprocessing.R
+
+class MediaCodecActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_media_codec)
+
+
+    }
+}
