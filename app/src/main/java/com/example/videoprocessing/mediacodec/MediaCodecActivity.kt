cfcbb61839c4a4ca7019f62f6b36c8fdb2e3c907
Parkduksung <duksung1234@naver.com>
2021-03-18 17:15:42 +0900

    [mediaCodec] 비율로 움직이기 때문에 찾을수 없다.

--- app/src/main/java/com/example/videoprocessing/mediacodec/MediaCodecActivity.kt
+++ app/src/main/java/com/example/videoprocessing/mediacodec/MediaCodecActivity.kt
@@ -1,15 +1,43 @@
 package com.example.videoprocessing.mediacodec
 
+import android.graphics.Point
+import android.media.MediaCodecList
+import android.media.MediaFormat
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.example.videoprocessing.R
 
 class MediaCodecActivity : AppCompatActivity() {
 
+    companion object {
+        private const val MIME_AVC = "video/avc"
+    }
+
+    @RequiresApi(Build.VERSION_CODES.R)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_media_codec)
 
 
+        val point = Point()
+        val display = display?.getRealSize(point)
+
+        //찾지를 못한다...
+//        val mediaFormat = MediaFormat.createVideoFormat(MIME_AVC, 1080, 2265)
+        val mediaFormat = MediaFormat.createVideoFormat(MIME_AVC, 1080, 2283)
+
+        val mediaCodecList = MediaCodecList(MediaCodecList.ALL_CODECS)
+        val result = mediaCodecList.findDecoderForFormat(mediaFormat)
+
+        Log.d("결과", result?:"찾을수 없다.")
+
     }
+
+
 }
+
+
+
